fbd8d39a3af6e5b0dc2109dc817ccf599ded64a2
kdatti14 <kdatti@asu.edu>
2024-03-23 12:13:01 -0700

    Removed Division Function

--- Basic_funct.py
+++ Basic_funct.py
@@ -12,17 +12,13 @@ def multiply(x, y):
     """Function to multiply two numbers"""
     return x * y
 
-def divide(x, y):
-    """Function to divide two numbers"""
-    if y == 0:
-        return "Error! Cannot divide by zero!"
-    else:
-        return x / y
+'''Removed Division function'''
+
 
 # Function calls
 
 print("Addition:", add(5, 3))  # Output: 8
 print("Subtraction:", subtract(7, 2))  # Output: 5
 print("Multiplication:", multiply(4, 6))  # Output: 24
-print("Division:", divide(10, 2))  # Output: 5.0
+#print("Division:", divide(10, 2))  # Output: 5.0
 print("Division by zero:", divide(8, 0))  # Output: Error! Cannot divide by zero!
